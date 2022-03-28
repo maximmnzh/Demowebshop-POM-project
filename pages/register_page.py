6ce869b4334f00491652650b83b44ebb31fd6cf5
maximmnzh <maxim.mnzh@gmail.com>
2022-03-28 23:27:21 +0300

    added many functions

---
+++ pages/register_page.py
@@ -0,0 +1,42 @@
+import pytest
+from .locators import HeadersLocators
+from .locators import RegisterLocators
+from .base_page import BasePage
+from .locators import AccountLocators
+
+
+class RegisterPage(BasePage):
+
+    @pytest.mark.smoke
+    def should_be_register_field(self):
+        assert self.is_element_present(*RegisterLocators.REGISTER_FIELD), 'Register button not present'
+
+    def should_be_register_result_page(self):
+        expected_url = 'http://demowebshop.tricentis.com/registerresult/1'
+        actual_url = self.browser.current_url
+        assert expected_url == actual_url, 'registration failed: wrong url after registration attempt'
+
+    def register_new_user(self, first_name, last_name, email, password):
+        self.browser.find_element(*RegisterLocators.MALE_RADIOBUTTON).click()
+        self.browser.find_element(*RegisterLocators.FIRSTNAME_FIELD).send_keys(first_name)
+        self.browser.find_element(*RegisterLocators.LASTNAME_FIELD).send_keys(last_name)
+        self.browser.find_element(*RegisterLocators.EMAIL_FIELD).send_keys(email)
+        self.browser.find_element(*RegisterLocators.PASSWORD_FIELD).send_keys(password)
+        self.browser.find_element(*RegisterLocators.CONFIRM_PASSWORD_FIELD).send_keys(password)
+        self.browser.find_element(*RegisterLocators.REGISTER_BUTTON).click()
+
+    def log_in(self, email, password):
+        self.browser.find_element(*RegisterLocators.EMAIL_FIELD).send_keys(email)
+        self.browser.find_element(*RegisterLocators.PASSWORD_FIELD).send_keys(password)
+        self.browser.find_element(*RegisterLocators.LOG_IN_BUTTON).click()
+
+    def change_pass(self, old_pass, new_pass):
+        self.browser.find_element(*AccountLocators.OLD_PASS_FIELD).send_keys(old_pass)
+        self.browser.find_element(*AccountLocators.NEW_PASS_FIELD).send_keys(new_pass)
+        self.browser.find_element(*AccountLocators.NEW_PASS_REPEAT_FIELD).send_keys(new_pass)
+        self.browser.find_element(*AccountLocators.CHANGE_PASS_BUTTON).click()
+
+    def should_be_success_pwd_change(self):
+        result_message = self.browser.find_element(*AccountLocators.CHANGE_PASS_RESULT).text
+        print(result_message)
+        assert result_message == "Password was changed", 'result message is not looking good'
