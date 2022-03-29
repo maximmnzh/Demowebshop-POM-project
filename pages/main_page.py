84aa54f43ac76bacec0ca56912e8d25af6ef95a2
maximmnzh <maxim.mnzh@gmail.com>
2022-03-29 11:04:53 +0300

    added checks *should_be buttons*

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,17 @@
 from .base_page import BasePage
+from .locators import HeadersLocators
 
 
 class MainPage(BasePage):
-    def __init__(self, *args, **kwargs):
-        super(MainPage, self).__init__(*args, **kwargs)
 
+    def should_be_register_button(self):
+        assert self.is_element_present(*HeadersLocators.REGISTER_BUTTON), 'Register button not present'
+
+    def should_be_login_button(self):
+        assert self.is_element_present(*HeadersLocators.LOGIN_LINK), 'Login button is not present'
+
+    def should_be_cart_button(self):
+        assert self.is_element_present(*HeadersLocators.CART_LINK), 'Cart button is not present'
+
+    def should_be_wishlist_button(self):
+        assert self.is_element_present(*HeadersLocators.WISHLIST_BUTTON), 'Wishlist button is not present'
