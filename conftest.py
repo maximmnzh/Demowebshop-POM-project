364a04576ccf48b648116a2d1ec8fa2a7163decc
maximmnzh <maxim.mnzh@gmail.com>
2022-03-31 14:54:27 +0300

    added browser selection and headless mode

--- conftest.py
+++ conftest.py
@@ -1,11 +1,28 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default=None,
+                     help="Choose browser: chrome or firefox")
 
 
 @pytest.fixture(scope="function")
-def browser():
-    browser = webdriver.Chrome()
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    if browser_name == "chrome":
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome(options=chrome_options)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     browser.implicitly_wait(5)
     yield browser
     print("\nquit browser..")
     browser.quit()
+
