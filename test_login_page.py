bc5426413e8d5d2e8f32b1619b027b1c11449d8d
maximmnzh <maxim.mnzh@gmail.com>
2022-04-04 12:03:55 +0300

    Authorization going with ready-made test data. The previous version worked incorrectly

--- test_login_page.py
+++ test_login_page.py
@@ -1,7 +1,6 @@
 import pytest
 from pages.locators import LinksLocators
 from pages.login_page import LoginPage
-from pages.register_page import RegisterPage
 
 
 @pytest.mark.fields
@@ -23,18 +22,9 @@ class TestLoginPageFields:
 
 
 @pytest.mark.smoke
-class TestLoginAfterRegistration:
-    @pytest.fixture(autouse=True)
-    def setup(self, browser):
-        page = RegisterPage(browser, LinksLocators.REGISTER_PAGE_LINK)
-        page.open()
-        first_name, last_name, email, password = page.register_dataset()
-        page.register_new_user(first_name, last_name, email, password)
-        page.should_be_register_result_page()
-        return email, password
-
-    def test_login(self, browser, setup):  # Проверка логина
+class TestLogin:
+    def test_login(self, browser):  # Проверка логина
         page = LoginPage(browser, LinksLocators.LOGIN_PAGE_LINK)
         page.open()
-        page.login(email=setup, password=setup)
+        page.login(email="test111222@test111222.com", password="test123")
         page.should_be_login_result_page()
